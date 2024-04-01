66ac1134b00813da81432a75281ca43db22241fb
Syfanadya Wening Adi <syfanadya5@gmail.com>
2024-04-01 14:23:59 +0700

    Memisahkan Composable dari Activity

--- app/src/main/java/org/d3if3128/asesmenmobpro/MainActivity.kt
+++ app/src/main/java/org/d3if3128/asesmenmobpro/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import org.d3if3128.asesmenmobpro.ui.screen.MainScreen
 import org.d3if3128.asesmenmobpro.ui.theme.AsesmenMobproTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   MainScreen()
                 }
             }
         }
