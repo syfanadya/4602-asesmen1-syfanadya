f16ad0e08adf2ffee8fdfae29e03ac2cb63cf31f
Syfanadya Wening Adi <syfanadya5@gmail.com>
2024-04-03 12:56:34 +0700

    Meminta input data dari pengguna

--- app/src/main/java/org/d3if3128/asesmenmobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3128/asesmenmobpro/ui/screen/MainScreen.kt
@@ -1,20 +1,41 @@
 package org.d3if3128.asesmenmobpro.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import org.d3if3128.asesmenmobpro.R
 import org.d3if3128.asesmenmobpro.ui.theme.AsesmenMobproTheme
 
@@ -41,14 +62,101 @@ fun MainScreen(){
 
 @Composable
 fun ScreenContent(modifier: Modifier){
+    var nama by remember { mutableStateOf("") }
+    var berat by remember { mutableStateOf("") }
+    var tinggi by remember { mutableStateOf("") }
+
+    var expanded by remember { mutableStateOf(false) }
+    val list = listOf("0 bulan", "1 bulan", "2 bulan", "3 bulan", "4 bulan", "5 bulan", "6 bulan",
+        "7 bulan", "8 bulan", "9 bulan", "10 bulan", "11 bulan", "12 bulan",
+        "13 bulan", "14 bulan", "15 bulan", "16 bulan", "17 bulan", "18 bulan",
+        "19 bulan", "20 bulan", "21 bulan", "22 bulan", "23 bulan", "24 bulan")
+    var pilihUsia by remember { mutableStateOf("") }
+    var textFiledSize by remember { mutableStateOf(Size.Zero) }
+    val icon = if (expanded){
+        Icons.Filled.KeyboardArrowUp
+    }else{
+        Icons.Filled.KeyboardArrowDown
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ){
-
+        Text(
+            text = stringResource(id = R.string.nutribaby_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = nama,
+            onValueChange = { nama = it },
+            label = { Text(text = stringResource(R.string.nama_bayi)) },
+            singleLine = false,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = pilihUsia,
+            onValueChange = {pilihUsia = it},
+            readOnly = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    textFiledSize = coordinates.size.toSize()
+                },
+            label = { Text(text = stringResource(R.string.usia_bayi))},
+            trailingIcon = {
+                Icon(icon,"", Modifier.clickable { expanded = !expanded } )
+            }
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false},
+            modifier = Modifier.size(width = 328.dp, height = 300.dp)
+        ) {
+            list.forEach { label ->
+                DropdownMenuItem(text = { Text(text = label) },
+                    onClick = {
+                        pilihUsia = label
+                        expanded = false
+                    }
+                )
+            }
+        }
+        OutlinedTextField(
+            value = berat,
+            onValueChange = { berat = it },
+            label = { Text(text = stringResource(R.string.berat_badan)) },
+            trailingIcon = { Text(text = "kg") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = tinggi,
+            onValueChange = { tinggi = it },
+            label = { Text(text = stringResource(R.string.tinggi_badan)) },
+            trailingIcon = { Text(text = "cm") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
