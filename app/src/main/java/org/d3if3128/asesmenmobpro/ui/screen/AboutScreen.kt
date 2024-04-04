4b6886ed256a1818cc7983101b321642390d3d44
Syfanadya Wening Adi <syfanadya5@gmail.com>
2024-04-04 14:37:11 +0700

    Membuat navigasi dengan tombol Up dan menangani bug icon material

--- app/src/main/java/org/d3if3128/asesmenmobpro/ui/screen/AboutScreen.kt
+++ app/src/main/java/org/d3if3128/asesmenmobpro/ui/screen/AboutScreen.kt
@@ -2,7 +2,11 @@ package org.d3if3128.asesmenmobpro.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,15 +17,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import org.d3if3128.asesmenmobpro.R
 import org.d3if3128.asesmenmobpro.ui.theme.AsesmenMobproTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutScreen() {
+fun AboutScreen(navController: NavHostController) {
     Scaffold(
         topBar = {
             TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector =Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.kembali),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
                 title = {
                     Text(text = stringResource(id = R.string.tentang_aplikasi))
                 },
@@ -47,6 +62,6 @@ fun AboutScreen() {
 @Composable
 fun AboutPreview() {
     AsesmenMobproTheme {
-        AboutScreen()
+        AboutScreen(rememberNavController())
     }
 }
