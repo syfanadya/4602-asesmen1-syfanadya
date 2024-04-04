414b9a8a5e76d39f8a2f85eb35b3f95760e83ac9
Syfanadya Wening Adi <syfanadya5@gmail.com>
2024-04-04 14:19:43 +0700

    Membuat navigasi menggunakan menu

--- app/src/main/java/org/d3if3128/asesmenmobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3128/asesmenmobpro/ui/screen/MainScreen.kt
@@ -20,12 +20,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -51,12 +53,15 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import org.d3if3128.asesmenmobpro.R
+import org.d3if3128.asesmenmobpro.navigation.Screen
 import org.d3if3128.asesmenmobpro.ui.theme.AsesmenMobproTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen() {
+fun MainScreen(navController: NavHostController) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -66,7 +71,18 @@ fun MainScreen() {
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
-                )
+                ),
+                actions = {
+                    IconButton(onClick = {
+                            navController.navigate(Screen.About.route)
+                    }) {
+                        Icon(
+                            imageVector = Icons.Outlined.Info,
+                            contentDescription = stringResource(R.string.tentang_aplikasi),
+                            tint = MaterialTheme.colorScheme.primary
+                            )
+                    }
+                }
             )
         }
     ) { padding ->
@@ -510,6 +526,6 @@ private fun getKategoriGizi(berat: Float, isMale: Boolean, pilihUsia: String): I
 @Composable
 fun ScreenPreview() {
     AsesmenMobproTheme {
-        MainScreen()
+        MainScreen(rememberNavController())
     }
 }
